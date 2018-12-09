cc1f4c0363c1758e69d2ddf77b11a7a895bdd9d9
jackalhan <jackalhan@gmail.com>
2018-12-08 23:36:03 -0600

    -squad sh is updated.

--- examples/run_for_squad.sh
+++ examples/run_for_squad.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 path="/home/jackalhan/Development/github/MatchZoo/examples/squad/dev_no_lower_no_filter/"
-sample="$pathsample.txt"
-glove="$pathglove.840B.300d.txt"
-word_dict="$pathword_dict.txt"
-embed_glove="$pathembed_glove_d300"
-normed_glove="$pathembed_glove_d300_norm"
+#glove="${path}glove.840B.300d.txt"
+#word_dict="${path}word_dict.txt"
+#embed_glove="${path}embed_glove_d300"
+#normed_glove="${path}embed_glove_d300_norm"
 
-python test_preparation_for_ranking.py --data_path=$path
+#python test_preparation_for_ranking.py --data_path=${path}
 
-python gen_w2v.py $glove $word_dict $embed_glove
+#python gen_w2v.py ${glove} ${word_dict} ${embed_glove}
 
-python norm_embed.py $glove $normed_glove
+#python norm_embed.py ${embed_glove} ${normed_glove}
 
-cd squad/dev_no_lower_no_filter/
-cat word_stats.txt | cut -d ' ' -f 1,4 > embed.idf
-cd ..
-cd ..
+#cd squad/dev_no_lower_no_filter/
+#cat word_stats.txt | cut -d ' ' -f 1,4 > embed.idf
+#cd ..
+#cd ..
 
-python gen_hist4drmm.py 60 $path
+python gen_hist4drmm.py 60 ${path}
