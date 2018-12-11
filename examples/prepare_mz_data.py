cbe3c9751aca3e9e6a7bbd48aa141f095d3d5448
jackalhan <jackalhan@gmail.com>
2018-12-11 09:26:33 -0600

    -additional_confgis for squad

--- examples/prepare_mz_data.py
+++ examples/prepare_mz_data.py
@@ -9,8 +9,8 @@ random.seed(49999)
 import numpy
 numpy.random.seed(49999)
 
-sys.path.append('../../matchzoo/inputs/')
-sys.path.append('../../matchzoo/utils/')
+sys.path.append('../matchzoo/inputs/')
+sys.path.append('../matchzoo/utils/')
 
 from preparation import Preparation
 from preprocess import Preprocess, NgramUtil
@@ -45,35 +45,9 @@ def filter_triletter(tri_stats, min_filter_num=5, max_filter_num=10000):
 
 if __name__ == '__main__':
     prepare = Preparation()
-    srcdir = './'
-    dstdir = './'
+    srcdir = sys.argv[2]
+    dstdir = srcdir
 
-    infiles = [ srcdir + 'WikiQA-mz-train.txt', srcdir + 'WikiQA-mz-dev.txt', srcdir + 'WikiQA-mz-test.txt']
-    corpus, rel_train, rel_valid, rel_test = prepare.run_with_train_valid_test_corpus(infiles[0], infiles[1], infiles[2])
-    print('total corpus : %d ...' % (len(corpus)))
-    print('total relation-train : %d ...' % (len(rel_train)))
-    print('total relation-valid : %d ...' % (len(rel_valid)))
-    print('total relation-test: %d ...' % (len(rel_test)))
-    prepare.save_corpus(dstdir + 'corpus.txt', corpus)
-
-    prepare.save_relation(dstdir + 'relation_train.txt', rel_train)
-    prepare.save_relation(dstdir + 'relation_valid.txt', rel_valid)
-    prepare.save_relation(dstdir + 'relation_test.txt', rel_test)
-    print('Preparation finished ...')
-
-    preprocessor = Preprocess(word_stem_config={'enable': False}, word_filter_config={'min_freq': 2})
-    dids, docs = preprocessor.run(dstdir + 'corpus.txt')
-    preprocessor.save_word_dict(dstdir + 'word_dict.txt', True)
-    preprocessor.save_words_stats(dstdir + 'word_stats.txt', True)
-
-    fout = open(dstdir + 'corpus_preprocessed.txt', 'w')
-    for inum, did in enumerate(dids):
-        fout.write('%s %s %s\n' % (did, len(docs[inum]), ' '.join(map(str, docs[inum]))))
-    fout.close()
-    print('Preprocess finished ...')
-
-    # dssm_corp_input = dstdir + 'corpus_preprocessed.txt'
-    # dssm_corp_output = dstdir + 'corpus_preprocessed_dssm.txt'
     word_dict_input = dstdir + 'word_dict.txt'
     triletter_dict_output = dstdir + 'triletter_dict.txt'
     word_triletter_output = dstdir + 'word_triletter_map.txt'
